6ccad7936a2737d3aa8f6e08f6ad5cd0cb658711
DawsonSilkenat <Dawsonsilkenat@gmail.com>
2020-11-17 12:36:22 +0000

    thresholding with normalised rgb image

--- src/image1.py
+++ src/image1.py
@@ -42,14 +42,15 @@ class image_converter:
     #cv2.imwrite('image_copy.png', cv_image)
 
     # Used to test rgb_normalization function
-    rgb_normalize(self.cv_image1)
+    image = rgb_normalize(self.cv_image1)
 
-    c = detect_orange_center(self.cv_image1)
+    c = detect_orange_center(image)
     self.cv_image1[c[1] - 1:c[1] + 1, c[0] - 1: c[0] + 1, 0] = 0
     self.cv_image1[c[1] - 1:c[1] + 1, c[0] - 1: c[0] + 1, 1] = 0
     self.cv_image1[c[1] - 1:c[1] + 1, c[0] - 1: c[0] + 1, 2] = 255
 
-    im1=cv2.imshow('window1', self.cv_image1)
+    cv2.imshow('window1', self.cv_image1)
+    # cv2.imshow('window2', image)
     cv2.waitKey(1)
     # Publish the results
     try: 
@@ -85,15 +86,16 @@ def rgb_normalize(image):
   rgb_norm_image = cv2.convertScaleAbs(rgb_norm_image)
 
   # imshow used for testing purposes:
-  cv2.imshow('normalization', rgb_norm_image)
-  cv2.waitKey(1)
+  # cv2.imshow('normalization', rgb_norm_image)
+  # cv2.waitKey(1)
 
   return rgb_norm_image
 
+
 def detect_yellow_center(image):
   # Color boundaries
-  yellow_lower = np.array([0,75,75])
-  yellow_upper = np.array([30,255,255])
+  yellow_lower = np.array([0,100,100])
+  yellow_upper = np.array([10,150,150])
   # Thresholding and removing noise
   thresholded = cv2.inRange(image, yellow_lower, yellow_upper)
   thresholded = cv2.erode(thresholded, np.ones(3, np.uint8))
@@ -106,8 +108,8 @@ def detect_yellow_center(image):
 
 def detect_blue_center(image):
   # Color boundaries
-  blue_lower = np.array([75,0,0])
-  blue_upper = np.array([255,75,75])
+  blue_lower = np.array([200,0,0])
+  blue_upper = np.array([255,10,10])
   # Thresholding and removing noise
   thresholded = cv2.inRange(image, blue_lower, blue_upper)
   thresholded = cv2.erode(thresholded, np.ones(3, np.uint8))
@@ -120,8 +122,8 @@ def detect_blue_center(image):
     
 def detect_green_center(image):
   # Color boundaries
-  green_lower = np.array([0,75,0])
-  green_upper = np.array([75,255,75])
+  green_lower = np.array([0,200,0])
+  green_upper = np.array([10,255,10])
   # Thresholding and removing noise
   thresholded = cv2.inRange(image, green_lower, green_upper)
   thresholded = cv2.erode(thresholded, np.ones(3, np.uint8))
@@ -134,8 +136,8 @@ def detect_green_center(image):
 
 def detect_red_center(image):
   # Color boundaries
-  red_lower = np.array([0,0,100])
-  red_upper = np.array([50,50,255])
+  red_lower = np.array([0,0,180])
+  red_upper = np.array([10,10,255])
   # Thresholding and removing noise
   thresholded = cv2.inRange(image, red_lower, red_upper)
   thresholded = cv2.erode(thresholded, np.ones(3, np.uint8))
@@ -148,31 +150,31 @@ def detect_red_center(image):
 
 def detect_orange_center(image):
   # Color boundaries
-  orange_lower = np.array([0,50,50])
-  orange_upper = np.array([50,80,255])
+  orange_lower = np.array([0,50,100])
+  orange_upper = np.array([10,100,175])
   # Thresholding and removing noise
   thresholded = cv2.inRange(image, orange_lower, orange_upper)
   thresholded = cv2.erode(thresholded, np.ones(3, np.uint8))
   thresholded = cv2.dilate(thresholded, np.ones(3, np.uint8))
   
   ones = np.argwhere(thresholded)
-  y_max = np.max(ones[:,1])
-  y_min = np.min(ones[:,1])
-  x_max = np.max(ones[:,0])
-  x_min = np.min(ones[:,0])
+  y_max = np.max(ones[:,0]) + 10
+  y_min = np.min(ones[:,0]) - 10
+  x_max = np.max(ones[:,1]) + 10
+  x_min = np.min(ones[:,1]) - 10
 
-  cv2.imshow("all", thresholded)
+  # cv2.imshow("all", thresholded)
 
-  thresholded = thresholded[x_min - 10 : x_max + 10, y_min - 10: y_max + 10]
+  thresholded = thresholded[y_min : y_max, x_min : x_max]
 
-  cv2.imshow("target", thresholded)
-  cv2.waitKey(1)
+  # cv2.imshow("target", thresholded)
+  # cv2.waitKey(1)
 
   # Finding the center point
   moments = cv2.moments(thresholded)
   cx = int(moments['m10']/moments['m00'])
   cy = int(moments['m01']/moments['m00'])
-  return np.array([cx,cy])
+  return np.array([cx + x_min, cy + y_min])
 
 
 # call the class
